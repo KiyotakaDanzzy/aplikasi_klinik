c31e42418ce0d6e45dff29395923324505b4d3e5
KiyotakaDanzzy <emperory.08@gmail.com>
2025-07-27 21:10:31 +0700

    Initial commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,30 @@
+<!--Start Footer-->
+<!-- Footer Start -->
+<footer class="footer text-center text-sm-start">
+    &copy; <script>
+        document.write(new Date().getFullYear())
+    </script> Metrica <span class="text-muted d-none d-sm-inline-block float-end">Crafted with <i
+            class="mdi mdi-heart text-danger"></i> by Mannatthemes</span>
+</footer>
+<!-- end Footer -->
+<!--end footer-->
+</div>
+<!-- end page content -->
+</div>
+<!-- end page-wrapper -->
+<!-- Javascript  -->
+<!-- vendor js -->
+<script src="<?php echo base_url(); ?>assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/libs/simplebar/simplebar.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/libs/feather-icons/feather.min.js"></script>
+<!-- Sweet-Alert  -->
+<script src="<?php echo base_url(); ?>assets/libs/sweetalert2/sweetalert2.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/pages/sweet-alert.init.js"></script>
+<!-- App js -->
+<script src="<?php echo base_url(); ?>assets/js/app.js"></script>
+<!-- Custom -->
+<script src="<?php echo base_url() ?>/assets/js/pagination.js"></script>
+<script src="<?php echo base_url() ?>/assets/js/js-form.js"></script>
+</body>
+<!--end body-->
+</html>
